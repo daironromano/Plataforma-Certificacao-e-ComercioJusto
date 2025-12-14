19b998e1ca730aa344a66d0e366fbdffa709345d
daironromano <daironromano@gmail.com>
2025-12-13 21:35:54 -0300

    feat: reestruturação do banco de dados

--- Códigos/Banco/banco_de_dados.sql
+++ Códigos/Banco/banco_de_dados.sql
@@ -5,64 +5,48 @@ USE amazonia_marketing;
 -- 1. Tabela Pai (Superclasse)
 -- Guarda os dados comuns a todos os perfis.
 CREATE TABLE Usuarios (
+    -- Dados comuns
 	id_usuario INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     telefone VARCHAR(20),
     endereco VARCHAR(100) NOT NULL,
-    senha VARCHAR(100) NOT NULL
+    senha VARCHAR(100) NOT NULL,
+    -- Define quem é quem
+    tipo ENUM('produtor', 'empresa', 'admin') NOT NULL,
+    -- Dados específicos
+    cpf VARCHAR(14) NULL,
+    cnpj VARCHAR(18) NULL,
+    matricula VARCHAR(12) NULL
+
 );
 
--- 2. Tabelas Filhas (Subclasses) - Implementação da Herança
--- Note que a PK também é FK. Elas não são auto_increment. 
-CREATE TABLE Produtores (
-	id_usuario INT PRIMARY KEY,
-    cpf VARCHAR(14) NOT NULL,
-    registro_rural VARCHAR(50),
--- O "ON DELETE CASCADE" garante que se apagar o Usuario, apaga o Produtor também.
-	FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario) ON DELETE CASCADE
-);
-
-CREATE TABLE Empresas (
-	id_usuario INT PRIMARY KEY,
-    cnpj VARCHAR(18) NOT NULL,
-    razao_social VARCHAR(100),
-    FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario) ON DELETE CASCADE
-);
-
-CREATE TABLE Admins (
-	id_usuario INT PRIMARY KEY,
-    setor VARCHAR(50),
-    nivel_acesso INT,
-    FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario) ON DELETE CASCADE
-);
-
--- 3. Tabela Produtos
+-- 2. Tabela Produtos
 CREATE TABLE Produtos (
 	id_produto INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     categoria VARCHAR(100) NOT NULL,
     descricao TEXT,
-    preco DECIMAL(10, 2), -- 10 dígitos no total, sendo 2 casas decimais
+    preco DECIMAL(10, 2), 
     status_estoque ENUM('disponivel', 'esgotado') DEFAULT 'disponivel',
+    -- Quem cadastrou o produtor
     usuario_id INT NOT NULL,
     FOREIGN KEY (usuario_id) REFERENCES Usuarios(id_usuario)
 );
 
--- 4. Tabela Certificações
--- Atenção para a FK do Admin
+-- 3. Tabela Certificações
 CREATE TABLE Certificacoes (
 	id_certificacao INT AUTO_INCREMENT PRIMARY KEY,
+    -- Autodeclaração
     texto_autodeclaracao TEXT,
     documento VARCHAR(255) NOT NULL,
     status_certificacao ENUM('pendente', 'aprovado', 'reprovado') DEFAULT 'pendente',
     data_envio DATE,
     data_resposta DATE,
     produto_id INT NOT NULL,
-    admin_responsavel_id INT, -- Pode ser NULL se ninguém avaliou ainda. 
+    admin_responsavel_id INT,
     FOREIGN KEY (produto_id) REFERENCES Produtos(id_produto),
--- Aqui está a correção crucial: Referencia a tabela Admins, não usuarios genéricos. 
-	FOREIGN KEY (admin_responsavel_id) REFERENCES Admins(id_usuario)
+	FOREIGN KEY (admin_responsavel_id) REFERENCES Usuarios(id_usuario)
 );
 
 -- 5. Tabela Marketplace
